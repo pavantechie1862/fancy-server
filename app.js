b2380d6e4e50405cc7883f9ad8e543f61eb20410
pavantechie1862 <pavanmarapalli171862@gmail.com>
2024-05-18 00:31:27 +0530

    removed cors

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(express.static("public"));
-app.use(cors());
+// app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/public", express.static(path.join(__dirname, "public")));
